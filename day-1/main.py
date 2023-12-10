eab2515a4ceda033da3ec119ec9e71043327c722
pre <preimmortal@gmail.com>
2023-12-09 21:19:19 -0800

    update day 1 and remove trie

--- day-1/main.py
+++ day-1/main.py
@@ -1,24 +1,5 @@
 from collections import defaultdict
 
-class Node:
-    def __init__(self):
-        self.children = {}
-        self.end = False
-
-class Trie:
-    def __init__(words):
-        self.root = Node()
-        for word in words:
-            self.insert(word)
-    
-    def insert(self, word):
-        node = self.root
-        for c in word:
-            if c not in node.children:
-                node.children[c] = Node()
-            node = node.children[c]
-        node.end = True
-
 if __name__ == "__main__":
     def part1():
         res = 0
